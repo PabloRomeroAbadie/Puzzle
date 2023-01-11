335bc110f9963b1941120f9e72addaf7749af00c
PabloRomeroAbadie <pabloabadie1995@gmail.com>
2023-01-11 16:00:47 -0300

    se realizo la funcion para desordenar el puzzle y cuando se lo ordene correctamente, felicitar al usuario con graficos de serpentinas

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-let matrix = [
-    ["1","2","3"],
-    ["4","5","6"],
-    ["7","8",""]
-]
+let matrix = shuffleMatrix();
 
 let board = document.querySelector(".board");
 
@@ -15,6 +11,7 @@ addEventListeners();
 
 
 function drawTokens (){
+    board.innerHTML = "";
     matrix.forEach(row => row.forEach(element=>{
         if(element == ""){
             board.innerHTML += `<div class="empty">${element}</div>`
@@ -29,15 +26,136 @@ function addEventListeners(){
     tokens.forEach(token=>token.addEventListener("click", ()=>{
 
         let actualPosition = searchPosition(token.innerText);
-        console.log(actualPosition)
+        let emptyPosition = searchPosition("");
+        let movement = canItMove(actualPosition, emptyPosition);
+        
+        if(movement !== false){
+            updateMatrix(token.innerText, actualPosition, emptyPosition);
+        }
+
+        let result= compareMatrix();
+        
+        if(result == true){
+            confetti({
+                particleCount: 150,
+                spread: 180 
+              });
+        }
+
+        drawTokens();
+
+        addEventListeners();
     }))
 }
 
 function searchPosition(element){
+
+    let rowIndex = 0;
+    let columIndex = 0;
+
     matrix.forEach((row,index)  => {
         let rowElement = row.findIndex(item => item == element)
         if(rowElement !== -1){
+            rowIndex = index;
+            columIndex = rowElement;
         }
     })
-    return [index, rowElement]
+    return [rowIndex, columIndex]
 } 
+
+function canItMove(actualPosition, emptyPosition){
+
+    if(actualPosition[1] == emptyPosition[1]){
+        if(actualPosition[0]-emptyPosition[0] > 1 || actualPosition[0]-emptyPosition[0] < -1){
+            return false
+        }
+    }else if(actualPosition[0] == emptyPosition[0]){
+        if(actualPosition[1]-emptyPosition[1] > 1 || actualPosition[1]-emptyPosition[1] < -1){
+            return false
+        }
+    }else{
+        return false
+    }
+} 
+    // if(actualPosition[1] == emptyPosition[1]){
+    //     if(actualPosition[0]-emptyPosition[0] > 1 && actualPosition[0]-emptyPosition[0] < -1){
+    //         return "no Move"
+    //     }
+    // //     if(actualPosition[0]-emptyPosition[0] == -1){
+    // //         return "down"
+    // //     }else if(actualPosition[0]-emptyPosition[0] == 1){
+    // //         return "up"
+    // //     }else{
+    // //         return "no Move"
+    // // }
+    // }else if(actualPosition[0] == emptyPosition[0]){
+    //         if(actualPosition[1]-emptyPosition[1] > 1 && actualPosition[1]-emptyPosition[1] < -1){
+    //             return "no Move"
+    //         }
+    // //     if(actualPosition[1]-emptyPosition[1] == -1){
+    // //         return "right"
+    // //     }else if(actualPosition[1]-emptyPosition[1] == 1){
+    // //         return "left"
+    // //     }else{
+    // //         return "no Move"
+    // // }
+    // // }else{
+    // //         return "no Move"
+    // // }
+    //  }
+
+
+
+function updateMatrix(element, actualPosition, emptyPosition){
+    matrix[actualPosition[0]][actualPosition[1]] = ""
+    matrix[emptyPosition[0]][emptyPosition[1]] = element
+}
+
+function shuffleMatrix(){
+
+    let shuffleMatrix = [
+        [],
+        [],
+        []
+    ]
+
+    let array = ["1","2","3","4","5","6","7","8",""]
+    let shuffleArray = array.sort(()=> Math.random() - 0.5);
+    
+    let column = 0;
+    let row = 0;
+
+    shuffleArray.forEach(element => {
+        shuffleMatrix[row].push(element)
+        if(column < 2){
+            column++
+        }else{
+            column = 0;
+            row++
+        }
+    })
+
+    return shuffleMatrix;
+}
+
+function compareMatrix(){
+    let counter = 0;
+    let finalMatrix = [
+        ["1","2","3"],
+        ["4","5","6"],
+        ["7","8",""]
+    ]
+    matrix.forEach((row, indexRow)=> {
+        row.forEach((element, indexColumn) => {
+            if(element == finalMatrix[indexRow][indexColumn]){
+                counter++
+            }   
+        })
+    })
+    if(counter == 9){
+        return true;
+    }else{
+        false
+    }
+}
+
